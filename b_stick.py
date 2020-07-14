b3f6afecdc1a588463899cb4e8f8e29b7fb06f16
Corey Lehman <coreylehman@me.com>
2020-07-13 21:31:27 -0400

    Made neutral b hold for less time.

--- b_stick.py
+++ b_stick.py
@@ -19,6 +19,7 @@ class BStick(object):
         self._b_down = DelayedState(delay=0.0, minimum_hold_time=0.034)
         self._b_up = DelayedState(delay=0.0, minimum_hold_time=0.050)
         self._activation_time = 0.0
+        self._axis_hold_duration = 0.050
 
     def update(self, b_neutral, b_left, b_right, b_down, b_up, ls_x_raw, ls_x, ls_y):
         self.x_axis_output = ls_x
@@ -30,13 +31,19 @@ class BStick(object):
         if self._b_up.just_activated:
             self._activation_time = time.perf_counter()
             self.b_state.delay = 0.017
+            self._axis_hold_duration = 0.050
 
         if self._b_down.just_activated \
         or self._b_left.just_activated \
-        or self._b_right.just_activated \
-        or self._b_neutral.just_activated:
+        or self._b_right.just_activated:
             self._activation_time = time.perf_counter()
             self.b_state.delay = 0.0
+            self._axis_hold_duration = 0.050
+
+        if self._b_neutral.just_activated:
+            self._activation_time = time.perf_counter()
+            self.b_state.delay = 0.0
+            self._axis_hold_duration = 0.025
 
         self.b_state.is_active = self._b_neutral.is_active \
                               or self._b_left.is_active or self._b_right.is_active \
@@ -45,7 +52,7 @@ class BStick(object):
         self._x_axis.update(self._b_left.is_active, self._b_right.is_active)
         self._y_axis.update(self._b_down.is_active, self._b_up.is_active)
 
-        if time.perf_counter() - self._activation_time <= 0.050:
+        if time.perf_counter() - self._activation_time <= self._axis_hold_duration:
             should_bias_x = self._b_down.is_active or self._b_up.is_active
             x_bias = 0.35 * ls_x_raw if should_bias_x else 0.0
             self.x_axis_output = self._x_axis.value + x_bias
